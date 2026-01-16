0125f3c96e73c8cf63041ab1a812a5e30cc0f955
Piyush Kumar <68198876+Pi-commits@users.noreply.github.com>
2026-01-16 18:56:53 +0530

    Delete testing.py

--- testing.py
+++
@@ -1,9 +0,0 @@
-def main():
-    # This is a simple print statement
-    print("Hello! This is running from your personal GitHub setup.")
-    
-    name = "Piyush"
-    print(f"Current User: {name}")
-
-if __name__ == "__main__":
-    main()
