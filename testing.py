1c949320df28cf40b51e671f75b0d28d3a8352da
Piyush Kumar <68198876+Pi-commits@users.noreply.github.com>
2026-01-16 18:54:44 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,9 @@
+def main():
+    # This is a simple print statement
+    print("Hello! This is running from your personal GitHub setup.")
+    
+    name = "Piyush"
+    print(f"Current User: {name}")
+
+if __name__ == "__main__":
+    main()
